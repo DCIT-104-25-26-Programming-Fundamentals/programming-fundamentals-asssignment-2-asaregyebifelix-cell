84436323a456375673e0d14b6ff74b7c89fa0df1
asaregyebifelix-cell <asaregyebifelix@gmail.com>
2026-07-31 18:52:59 +0000

    complete prime number checker with isPrime function

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// Function to check if a number is prime
+// Optimized function to check if a number is prime
 bool isPrime(int num) {
-    if (n <= 1) return false
-    if (n <= 3) return true
-    if (n% 2 == 0 || n % 3 == 0) return false;
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
 
-    for (int i = 5; i * i <= n; i += 6){
-        if (n % i == 0 || n % (i + 2) == 0)
-
-        return false;
-    }
-    return true;
-    // Numbers less than 2 are not prime
-    if (num < 2) {
-        return false;
-    }
-    
-    // Check for divisors from 2 to num-1
-    for (int i = 2; i < num; i++) {
-        if (num % i == 0) {
-            return false;  // Found a divisor, not prime
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) {
+            return false;
         }
     }
-    
-    return true;  // No divisors found, number is prime
+    return true;
 }
 
 int main() {
@@ -42,13 +29,11 @@ int main() {
     
     return 0;
 }g++ -std=c++11 assignment_01_prime_checker.cpp -o prime_checker
-
 ./prime_checker
- 7
-
+#7
 ./prime_checker
-
-10
-
+#10
 ./prime_checker
-13
+#13
+
+
